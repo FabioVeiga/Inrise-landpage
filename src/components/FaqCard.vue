28ca4d36c68fc2fbbc5828d290403287c7434d2c
Gustavo Lemos <gsvieira08@gmail.com>
2024-11-11 18:09:12 -0300

    Upload das mudanças pra mobile

--- src/components/FaqCard.vue
+++ src/components/FaqCard.vue
@@ -1,88 +1,87 @@
 <template>
-    <div
-      :class="['faq-card', { 'bg-[#BB59F9] text-white': isOpen, 'bg-white text-black': !isOpen }]"
-      @click="toggle"
-    >
-      <div class="faq-header my-[41px] ml-[60px] flex justify-between items-center">
-        <div class="flex items-center">
-          <h3 :class="{'text-white': isOpen, 'text-black': !isOpen}" class="text-6xl font-medium mr-[25px]">{{ number }}</h3>
-          <h3 :class="{'text-white': isOpen, 'text-black': !isOpen}" class="text-3xl font-medium">{{ question }}</h3>
-        </div>
-        <button
-          class="toggle-btn mx-[60px]"
-          @click.stop="toggle"
-        >
-          {{ isOpen ? '-' : '+' }}
-        </button> 
-      </div>
-      <transition name="fade">
-        <div class="mx-[60px]">
-            <div v-if="isOpen" class="separator h-[2px] my-[30px] bg-gray-300"></div>
-        </div>
-       </transition>
-
-      <transition name="fade">
-        <div v-if="isOpen" :class="{'text-white': isOpen, 'text-black': !isOpen}" class="text-lg font-normal mb-[30px] mx-[60px]" v-html="answer"></div>      </transition>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      number: String,
-      question: String,
-      answer: String
-    },
-    data() {
-      return {
-        isOpen: false
-      };
-    },
-    methods: {
-      toggle() {
-        this.isOpen = !this.isOpen;
-      },
-      formatNumber(num) {
-        return num < 10 ? `0${num}` : `${num}`;
+  <div
+    :class="[
+      'faq-card',
+      {
+        'bg-[#BB59F9] text-white': isOpen,
+        'bg-white text-black': !isOpen
       }
+    ]"
+    @click="toggle"
+  >
+    <div class="faq-header my-[20px] sm:my-[30px] md:my-[41px] sm:ml-[20px] md:ml-[60px] mx-4 flex justify-between items-center">
+      <div class="flex items-center">
+        <h3 :class="{'text-white': isOpen, 'text-black': !isOpen}" class="text-2xl sm:text-3xl md:text-6xl font-medium mr-[15px] sm:mr-[20px] md:mr-[25px]">{{ number }}</h3>
+        <h3 :class="{'text-white': isOpen, 'text-black': !isOpen}" class="text-sm sm:text-lg md:text-3xl font-medium">{{ question }}</h3>
+      </div>
+      <button
+        class="toggle-btn mx-[20px] sm:mx-[30px] md:mx-[60px] border border-black w-[40px] h-[40px] sm:w-[50px] sm:h-[50px] md:w-[58px] md:h-[58px] rounded-full text-2xl sm:text-3xl lg:text-4xl font-extrabold bg-white text-black flex items-center justify-center cursor-pointer"
+        @click.stop="toggle"
+      >
+        {{ isOpen ? '-' : '+' }}
+      </button>
+    </div>
+
+    <transition name="fade">
+      <div class="mx-[20px] sm:mx-[30px] md:mx-[60px]">
+        <div v-if="isOpen" class="separator h-[2px] my-[15px] sm:my-[20px] md:my-[30px] bg-gray-300"></div>
+      </div>
+    </transition>
+
+    <transition name="fade">
+      <div
+        v-if="isOpen"
+        :class="{'text-white': isOpen, 'text-black': !isOpen}"
+        class="text-xs sm:text-sm md:text-lg font-normal mb-[15px] sm:mb-[20px] md:mb-[30px] mx-[20px] sm:mx-[30px] md:mx-[60px] transition-all duration-300 ease-in-out"
+        v-html="answer"
+      ></div>
+    </transition>
+  </div>
+</template>
+
+
+
+
+<script>
+export default {
+  props: {
+    number: String,
+    question: String,
+    answer: String
+  },
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+    },
+    formatNumber(num) {
+      return num < 10 ? `0${num}` : `${num}`;
     }
   }
-  </script>
-  
-  <style scoped>
-  .faq-card {
-    cursor: pointer;
-    margin-bottom: 10px;
-    border-radius: 45px;
-    border: solid 1px;
-    border-bottom-width: 5px;
-    border-color: black;
-    width: 1234px;
-    transition: background-color 0.3s ease, color 0.3s ease;
-  }
-  
-  .toggle-btn {
-  background: none;
-  border: solid 1px black;
-  width: 58px;
-  height: 58px;
-  border-radius: 50%; 
-  font-size: 46px; 
-  font-weight: 800;
-  background-color: white;
-  color: black; 
-  display: flex;
-  align-items: center;
-  justify-content: center; 
-  cursor: pointer;
-  outline: none;
 }
-  
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity 0.3s;
-  }
-  .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
-    opacity: 0;
-  }
-  </style>
-  
+</script>
+
+
+<style scoped>
+.faq-card {
+  @apply cursor-pointer mb-2 rounded-[45px] border border-black border-b-4 w-full max-w-[350px] lg:max-w-[1234px] transition-colors ease-in-out duration-300;
+  border-radius: 1.5rem;
+}
+
+.toggle-btn {
+  @apply bg-none border border-black w-[40px] h-[40px] sm:w-[50px] sm:h-[50px] md:w-[58px] md:h-[58px] rounded-full text-2xl sm:text-3xl lg:text-4xl font-extrabold bg-white text-black flex items-center justify-center cursor-pointer outline-none;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter, .fade-leave-to{
+  opacity: 0;
+}
+</style>
