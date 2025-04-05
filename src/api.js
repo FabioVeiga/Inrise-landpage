b5ae07a0148d8f1494dc9ea8751fb42142c7b331
Nicolemos <gsvieira08@gmail.com>
2025-04-05 10:56:31 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://apiinriseservice.azurewebsites.net',  
+  baseURL: 'https://api-inrise-service.azurewebsites.net',  
   headers: {
     'Content-Type': 'application/json',
     'secret': 'naf9uafjh_+mcdsaIFD023', 
